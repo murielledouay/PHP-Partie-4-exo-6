06ce3fc0c42aad0139558b2c04eb6486ca544d62
murielledouay <guyard_murielle@orange.fr>
2020-05-12 12:32:55 +0200

    PHP Partie 4 exo 6

--- index.php
+++ index.php
@@ -1,34 +1,30 @@
 <?php
-//déclaration des 2 variables avec les valeur
-$object = 'Réunion';
-$message = 'Bonjour, nous allons faire le point sur les chiffres en cours.';
-?>
 
+
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-
 <head>
   <meta charset="utf-8"/>
-  <link rel="stylesheet" href="assets/css/style.css"/>
-  <title>Partie 1 exo 1</title>
+  <title>PHP Partie 4 exo 6</title>
 </head>
-
 <body>
-
-  <p>Objet :
-    <?php
-    //j'affiche avec echo la variable sur mon navigateur de l'objet
-    echo $object;
+  <?php
+  for ($numbers = 1; $numbers <= 100; $numbers++){
+    $restDivision = $numbers % 3;
+    $result = $numbers;
+    $division5 = $numbers % 5;
+    if ($restDivision == 0){
+    $result = 'Fizz';
+    }
+    if ($division5 == 0){
+    $result = 'Buzz';
+    }
+    if ($restDivision == 0 && $division5 == 0){
+    $result = 'FizzBuzz';
+    }
     ?>
-  </p>
-
-
-  <p>
-    <?php
-      //j'affiche avec echo la variable du message sur mon navigateur
-    echo $message;
-    ?>
-  </p>
-
+    <p><?php echo $result; ?></p>
+    <?php } ?>
 </body>
 </html>
